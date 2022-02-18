677bb53f113275935f274534bc1c32a2bfb10126
Vvilainess <phungtruonggiang2507@gmail.com>
2022-02-18 09:44:11 +0700

    added search items

--- src/components/auth/getTokenFromUrl.js
+++ src/components/auth/getTokenFromUrl.js
@@ -12,6 +12,7 @@ const SCOPES = [
     "user-read-playback-state",
     "user-top-read",
     "user-modify-playback-state",
+    "playlist-read-private",
 ];
 const SCOPES_URL_PARAMS = SCOPES.join(SPACER);
 
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -4,33 +4,42 @@ import { useStore } from "../components/Store";
 
 const Search = () => {
     const [{ categories }, dispatch] = useStore();
-    useEffect(() => {
-        console.log(categories);
-    });
+    useEffect(() => {});
     return (
         <div className="relative bg-[#121212]">
             <Header searchInput={true} />
-            <div className="px-9">
-                <div className="w-full">
+            <div className="px-9 py-7">
+                <div className="w-full pb-3">
                     <h1 className="capitalize font-bold text-2xl text-white">
                         browser all
                     </h1>
                 </div>
-                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg: grid-cols-6">
-                    {/* {categories.map((category, idx) => {
-                        return <></>
-
-                    })} */}
-                    <div className="relative w-48 h-48 truncate rounded-md mx-3">
-                        <p className="text-white text-xl font-bold capitalize top-0 left-0 p-2">
-                            Test
-                        </p>
-                        <img
-                            src="https://t.scdn.co/media/derived/toplists_11160599e6a04ac5d6f2757f5511778f_0_0_275_275.jpg"
-                            alt=""
-                            className="absolute origin-center rotate-[35deg] w-24 h-24 right-0 bottom-0 m-0"
-                        />
-                    </div>
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 sm:gap-5 md:gap-4 lg:gap-5">
+                    {categories.map((category, idx) => {
+                        var randomColor = Math.floor(
+                            Math.random() * 16777215
+                        ).toString(16);
+                        return (
+                            <div
+                                key={idx}
+                                className={
+                                    `bg-[#${randomColor}] ` +
+                                    "relative truncate rounded-md cursor-pointer after:content-[''] after:pb-[100%] after:block"
+                                }
+                            >
+                                <div>
+                                    <p className="block absolute box-border text-white text-xl font-bold break-words max-w-full top-0 left-0 p-2">
+                                        {category.name}
+                                    </p>
+                                    <img
+                                        src={category.icons?.[0].url}
+                                        alt=""
+                                        className="absolute origin-center rotate-[25deg] translate-x-[18%] translate-y-[-2%] w-24 h-24 right-0 bottom-0 m-0"
+                                    />
+                                </div>
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </div>
