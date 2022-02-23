6356abe354bbefacc6378cdfee969b36704cb2be
Phùng Trường Giang <phungtruonggiang2507@gmail.com>
2022-02-23 17:14:28 +0700

    small update

--- src/components/Header-components/Header.js
+++ src/components/Header-components/Header.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { actions, useStore } from "../Store";
 import LoginBtn from "../Feature-components/LoginBtn";
 import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
 import getData from "../GetAPI/Axios";
 
 const Header = ({ searchInput }) => {
@@ -9,18 +10,20 @@ const Header = ({ searchInput }) => {
     const handleInput = (e) => {
         dispatch(actions.setInput(e.target.value));
     };
-    const getSearchResult = () => {
-        getData(
-            `https://api.spotify.com/v1/search?q=${input}&type=album&include_external=audio?`,
-            token,
-            "GET"
-        ).then((response) => {
-            if (input) {
-                dispatch(actions.setSearchResult(response.data.albums.items));
-            }
-        });
-    };
     useEffect(() => {
+        const getSearchResult = () => {
+            getData(
+                `https://api.spotify.com/v1/search?q=${input}&type=album&include_external=audio?`,
+                token,
+                "GET"
+            ).then((response) => {
+                if (input) {
+                    dispatch(
+                        actions.setSearchResult(response.data.albums.items)
+                    );
+                }
+            });
+        };
         if (input) {
             getSearchResult();
         } else {
@@ -43,6 +46,14 @@ const Header = ({ searchInput }) => {
                             className="sm:w-64 md:w-96 px-11 py-2 rounded-full text-black text-ellipsis focus-visible:outline-none"
                         />
                         <AiIcons.AiOutlineSearch className="absolute top-0 left-0 h-full text-2xl ml-3" />
+                        {input && (
+                            <IoIcons.IoMdClose
+                                onClick={() => {
+                                    dispatch(actions.setInput(""));
+                                }}
+                                className="absolute top-0 right-0 h-full text-2xl mr-3 cursor-pointer"
+                            />
+                        )}
                         <div className=""></div>
                     </form>
                 </>
